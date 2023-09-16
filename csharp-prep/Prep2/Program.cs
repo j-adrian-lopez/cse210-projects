837b6def6114c612706fdf24f66e3b38f2b0de5e
j-adrian-lopez <adrj156@byui.edu>
2023-09-16 16:45:17 -0400

    Finished Prep 2 with core and stretch requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get the grade percentage and output letter grade
+        Console.Write("What is your grade percentage? ");
+        int percentage = int.Parse(Console.ReadLine());
+        
+        string grade;
+        string sign;
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        
+        else
+        {
+            grade = "F";
+        }
+
+        if (percentage < 60 || percentage >=93)
+        {
+            sign = "";
+        }
+        else if (percentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (percentage % 10 <=3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"You got an {grade}{sign}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course!");
+        }
+        else 
+        {
+            Console.WriteLine("You will pass the course next semester, don't give up!");
+        }
     }
 }
